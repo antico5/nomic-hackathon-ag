7442ee9c3c97f015392c225eafa71aba7b9786f9
Armando Andini <armando.andini@gmail.com>
2023-01-30 11:41:46 -0300

    update

--- contracts/HeirWallet.sol
+++ contracts/HeirWallet.sol
@@ -1,4 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract HeirWallet {}
+uint constant ALIVE = 1;
+
+contract HeirWallet {
+    uint public status;
+
+    mapping(address => bool) public heirs;
+
+    uint public lastOwnerCall;
+
+    uint public claimStarted;
+
+    mapping(address => mapping(address => bool)) heirsWithdrawn;
+
+    mapping(address => uint) originalAssetBalance;
+
+    uint public immutable inactivityThreshold;
+    uint public immutable vetoThreshold;
+
+    constructor(uint _inactivityThreshold, uint _vetoThreshold) {
+        inactivityThreshold = _inactivityThreshold;
+        vetoThreshold = _vetoThreshold;
+    }
+}
